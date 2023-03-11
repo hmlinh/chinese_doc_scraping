bdd26bedd286ed8476dbf9cbfe6d36cde89dce84
My Linh <85177785+hmlinh@users.noreply.github.com>
2023-03-11 18:05:52 +0100

    Add files via upload

---
+++ scripts/scrape_doc_2.py
@@ -0,0 +1,161 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+
+import os
+import re
+import time
+import pandas as pd
+
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
+
+
+
+begin = time.time()
+
+
+
+######################################################################################################
+""" Change the variables to get the desired documents """
+
+# create a folder to store the downloaded files, change the path name to your preference
+new_path = "files"
+
+# change the range to scrape more pages, the range is inclusive, the number is the page number
+pages = range(1, 2) 
+    
+# change "yuwen/1/list_11" in the url to scrape from different categories
+cat = "yuwen/1/list_11"
+
+
+
+######################################################################################################
+""" get list of book names from each page number """
+
+def get_table(url):
+    response = requests.get(url)
+    
+    assert response.status_code == 200, "Page not found"
+    
+    page = BeautifulSoup(response.content, 'html.parser')
+    
+    return page.find_all("table")
+
+
+list_tb = []
+# iterate through the page numbers to get the list of tables, change the range to scrape more pages
+for nb in pages:        
+    url = f"https://www.xkb1.com/{cat}_{nb}.html"    # link of pages to scrape from
+    html = get_table(url)
+    table = pd.read_html(str(html))[6]
+    table = table.drop(table.index[0:9])
+    table = table.dropna()
+
+    list_tb.append(table)
+    df = pd.concat(list_tb) 
+
+
+# get list of doc names
+doc_list = df.iloc[:, 0].to_list()
+
+print("Number of documents: " + str(len(doc_list)))
+
+
+
+######################################################################################################
+""" get list of download urls and list of their directories """ 
+
+def get_link(url):
+    response = requests.get(url)
+    response.encoding = 'GBK'
+    
+    assert response.status_code == 200, "Page not found"
+    
+    page = BeautifulSoup(response.content, 'html.parser')
+
+    list_doc_html = []
+    for x in doc_list:
+        doc = page.find_all(lambda tag: tag.name == "a" and x in tag.text)
+
+        for d in doc:
+            li = "https://www.xkb1.com"  + d.get('href')
+            list_doc_html.append(li)
+            
+    return list_doc_html
+
+
+# get list of download urls 
+download_urls = []
+# iterate through the page numbers to get the list of doc html, change the range to scrape more pages
+for nb in pages:
+    url = f"https://www.xkb1.com/{cat}_{nb}.html"    # link of pages to scrape from
+    list_doc_html = get_link(url)
+
+
+    # iterate through the list of doc hltm in each page number
+    for l in list_doc_html:
+        r = requests.get(l)
+        r.encoding = 'GBK'
+        soup = BeautifulSoup(r.text, 'html.parser')
+            
+        # iterate through the list of urls in each doc html and find the download url
+        for link in soup.find_all(lambda tag : tag.name== "a" and "进入下载地址列表" in tag.text):
+            doc_url_2 = "https://www.xkb1.com" + link.get('href')
+
+            r2 = requests.get(doc_url_2)
+            r2.encoding = 'GBK'
+            soup2 = BeautifulSoup(r2.text, 'html.parser')
+                    
+            # iterate through the list of urls in each doc html and find the download url
+            for link2 in soup2.find_all(lambda tag : tag.name=="a" and "下载地址一:点击下载" in tag.text):
+                downloaded_url = "https://www.xkb1.com" + link2.get('href')
+                download_urls.append(downloaded_url)
+
+# remove duplicated urls
+download_urls = list(dict.fromkeys(download_urls))
+
+# create a folder to store the downloaded files
+if not os.path.exists(new_path):
+    os.mkdir(new_path)
+
+# get the list of file directories from the download urls
+file_dir = []
+for x in download_urls:
+    name = re.search('id=(.*)&', x.rsplit('/', 1)[-1]).group(1) + ".rar"
+    filename = os.path.join(new_path, name)
+    file_dir.append(filename) 
+
+print("Number of download_urls: " + str(len(file_dir)))
+
+
+
+######################################################################################################
+""" download the files and save them in the newly created folder """
+
+# Download the file if it does not exist
+def download_url(args):
+    downloaded_url, filename = args[0], args[1]
+
+    if not os.path.isfile(filename):
+        urllib.request.urlretrieve(downloaded_url, filename)
+
+
+# Download multiple files in parallel
+def download_parallel(args):
+    cpus = cpu_count()
+    results = ThreadPool(cpus - 1).imap_unordered(download_url, args)
+    for result in results:
+        result
+
+
+download_parallel(zip(download_urls, file_dir))
+
+
+
+end = time.time()
+
+
+
+print("Download completed")
+print("Time taken: " + str(round((end - begin)/60, 2)) + " minute(s)")
